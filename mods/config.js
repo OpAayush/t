4ed4869b0f9d52480f3591e321bc0dbe4fb25dcf
Aayu5h <aayushmagrawal@gmail.com>
2025-11-19 18:48:29 +0530

    verision

--- mods/config.js
+++ mods/config.js
@@ -1,8 +1,8 @@
-const CONFIG_KEY = 'ytaf-configuration';
+const CONFIG_KEY = "ytaf-configuration";
 const defaultConfig = {
   enableAdBlock: true,
   enableSponsorBlock: true,
-  sponsorBlockManualSkips: ['intro', 'outro', 'filler'],
+  sponsorBlockManualSkips: ["intro", "outro", "filler"],
   enableSponsorBlockSponsor: true,
   enableSponsorBlockIntro: true,
   enableSponsorBlockOutro: true,
@@ -14,17 +14,17 @@ const defaultConfig = {
   videoSpeed: 1,
   enableDeArrow: true,
   enableDeArrowThumbnails: false,
-  focusContainerColor: '#0f0f0f',
-  routeColor: '#0f0f0f',
-  enableFixedUI: (window.h5vcc && window.h5vcc.tizentube) ? false : true,
-  enableHqThumbnails: false,
+  focusContainerColor: "#0f0f0f",
+  routeColor: "#0f0f0f",
+  enableFixedUI: window.h5vcc && window.h5vcc.tizentube ? false : true,
+  enableHqThumbnails: true,
   enableChapters: true,
   enableLongPress: true,
   enableShorts: true,
   dontCheckUpdateUntil: 0,
   enableWhoIsWatchingMenu: false,
   enableShowUserLanguage: true,
-  enableShowOtherLanguages: false
+  enableShowOtherLanguages: false,
 };
 
 let localConfig;
@@ -32,13 +32,18 @@ let localConfig;
 try {
   localConfig = JSON.parse(window.localStorage[CONFIG_KEY]);
 } catch (err) {
-  console.warn('Config read failed:', err);
+  console.warn("Config read failed:", err);
   localConfig = defaultConfig;
 }
 
 export function configRead(key) {
   if (localConfig[key] === undefined) {
-    console.warn('Populating key', key, 'with default value', defaultConfig[key]);
+    console.warn(
+      "Populating key",
+      key,
+      "with default value",
+      defaultConfig[key]
+    );
     localConfig[key] = defaultConfig[key];
   }
 
@@ -46,10 +51,12 @@ export function configRead(key) {
 }
 
 export function configWrite(key, value) {
-  console.info('Setting key', key, 'to', value);
+  console.info("Setting key", key, "to", value);
   localConfig[key] = value;
   window.localStorage[CONFIG_KEY] = JSON.stringify(localConfig);
-  configChangeEmitter.dispatchEvent(new CustomEvent('configChange', { detail: { key, value } }));
+  configChangeEmitter.dispatchEvent(
+    new CustomEvent("configChange", { detail: { key, value } })
+  );
 }
 
 export const configChangeEmitter = {
@@ -60,11 +67,11 @@ export const configChangeEmitter = {
   },
   removeEventListener(type, callback) {
     if (!this.listeners[type]) return;
-    this.listeners[type] = this.listeners[type].filter(cb => cb !== callback);
+    this.listeners[type] = this.listeners[type].filter((cb) => cb !== callback);
   },
   dispatchEvent(event) {
     const type = event.type;
     if (!this.listeners[type]) return;
-    this.listeners[type].forEach(cb => cb.call(this, event));
-  }
+    this.listeners[type].forEach((cb) => cb.call(this, event));
+  },
 };
